504deb7a30181f9a5a6e94af23d5180f014bb87d
Waseem AL Tamer <107763849+WaseemALTamer@users.noreply.github.com>
2025-06-06 09:01:03 +0100

    Add files via upload

--- Run.sh
+++ Run.sh
@@ -3,7 +3,7 @@
 # This linux
 
 # Activate the virtual environment
-source /home/waseem/MusicPlayerOnVC/myenv/bin/activate
+source /home/waseem/MusicPlayerOnVC/.enve/bin/activate
 
 # Run the Python script
 python /home/waseem/MusicPlayerOnVC/Main.py
